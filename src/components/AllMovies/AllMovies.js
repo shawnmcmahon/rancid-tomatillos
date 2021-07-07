f0505c28dab6cec4143247efd34c49cf6862a60a
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-07 15:31:15 -0600

    Render movie posters to DOM

--- src/components/AllMovies/AllMovies.js
+++ src/components/AllMovies/AllMovies.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Movie from '../Movie/Movie';
 
 const AllMovies = (props) => {
-
-    const moviePosters = props.movies.map(movie => {
+    console.log('props.movies', props.movies)
+    const moviePosters = props.movies.movies.map(movie => {
         return (
             <Movie
                 id={movie.id}
                 poster={movie.poster_path}
                 title={movie.title}
-                
+                rating={movie.average_rating}
             />
         )
     })
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,8 +3,10 @@ import './Movie.css';
 
 const Movie = (props) => {
     return (
-
-
+        <div>
+            <img src={props.poster} className="movie"/>
+            <h3>Rating: {props.rating.toFixed(1)}</h3>
+        </div>
     )
 }
 
