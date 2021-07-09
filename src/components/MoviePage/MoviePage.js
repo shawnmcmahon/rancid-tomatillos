233e8ecb5189c3418664cb4bebb64d0eaf0c3a3e
Bobby Vasquez <shootbv@gmail.com>
2021-07-08 19:08:03 -0600

    Add basic styling to single movie page

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -2,13 +2,21 @@ import React from "react"
 import "./MoviePage.css"
 
 const MoviePage = (props) => {
+
+  const styles = {
+  backgroundImage: `url(${props.backdrop})`
+};
+
+
+// <img className="backdrop" src={props.backdrop} alt={props.title} />
   return (
-    <div>
-      <img className="backdrop" src={props.backdrop} alt={props.title} />
-      <h1>{props.title}</h1>
-      <h2>{props.tagline}</h2>
-      <p>{!props.overview ? "No overview available" : props.overview}</p>
-      <button onClick={props.goBack}>Go Back</button>
+    <div className="background-image" style={styles}>
+      <div className="movie-info">
+        <h1>{props.title}</h1>
+        <h2>{props.tagline}</h2>
+        <p>{!props.overview ? "No overview available" : props.overview}</p>
+        <button onClick={props.goBack}>Go Back</button>
+      </div>
     </div>
   )
 }
