c057bd795eab0f6190e9c4c877e1e3c0d9f7757e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-11 11:19:04 -0600

    Add structure to Trailer component

---
+++ src/components/Trailer/Trailer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Trailer.css';
+
+
+class Trailer extends Component {
+  constructor()  {
+    super() 
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+  
+    )
+
+  }
+
+}
+
+export default Trailer; 
