d2d4358a02fdf11c63f11ab7542f6a610c88ee91
Bobby Vasquez <shootbv@gmail.com>
2021-07-12 17:34:33 -0600

    Add function to give slightly more accurate star rating

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -3,11 +3,23 @@ import "./Rating.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Rating = (props) => {
-  const rating = Math.floor((props.rating.toFixed(1) / 2))
-  const stars = new Array(rating).fill(<FontAwesomeIcon icon="star" className="checked" />)
-  const remainder = new Array(5 - rating).fill(<FontAwesomeIcon icon="star" />)
+  const checkNum = (num) => {
+    let float = parseInt(num.toString().split('.')[1])
+    if (float >= 5) {
+      return Math.ceil(num)
+    }
+    // else if (float > 5) {
+    //   return Math.ceil(num)
+    // }
+    return Math.floor(num)
+  }
+  const rating = (props.rating.toFixed(1) / 2)
+  const rounded = checkNum(rating)
+  const stars = new Array(rounded).fill(<FontAwesomeIcon icon="star" className="checked" />)
+  const remainder = new Array(5 - rounded).fill(<FontAwesomeIcon icon="star" />)
   const allStars = stars.concat(remainder)
   const copy = [...allStars]
+
   //
   // const indexedStars = copy.reduce((acc, currentVal, index) => {
   //   console.log(currentVal)
