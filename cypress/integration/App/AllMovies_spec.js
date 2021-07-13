b20923b216f22cc2103d26f980eeda9a08bfa928
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-13 17:12:30 -0600

    Add test for forward and back buttons

--- cypress/integration/App/AllMovies_spec.js
+++ cypress/integration/App/AllMovies_spec.js
@@ -1,4 +1,4 @@
-describe('User login', () => {
+describe('Rotten Tomatillos', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000');
@@ -29,7 +29,23 @@ describe('User login', () => {
       fixture: 'movies.json' 
     })
     cy.get("a[name='Money Plane']").find("img").should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
-    
+  })
+
+  it('Should be rendering Movie data on a single movie page', () => {
+    cy.visit('http://localhost:3000/337401')
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+      fixture: 'singleMovie.json' 
+    })
+    cy.get("img").should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg")
+    cy.contains("Action")
+  })
+
+  it('Should be able to use the back button within the browser', () => {
+    cy.visit('http://localhost:3000/337401')
+    cy.go('back')
+    cy.url().should('eq','http://localhost:3000/')
+    cy.go('forward')
+    cy.url().should('eq','http://localhost:3000/337401')
   })
 
 
