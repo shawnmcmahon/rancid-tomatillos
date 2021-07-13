aee5574aebb2396ba3cfb08a770951261e166364
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-13 16:57:08 -0600

    Add intercept test for AllMovies Heroku API

--- cypress/integration/App/AllMovies_spec.js
+++ cypress/integration/App/AllMovies_spec.js
@@ -10,12 +10,34 @@ describe('User login', () => {
         .contains('Rancid Tomatillos')
   })
 
-  it('Clicking on a movie poster should return a unique URL with the movies\' information', () => {
+  it('Should see a movie\' details when clicking on a movie poster', () => {
       cy.get("a[name='Mulan']")
         .click()
       cy.contains("When the Emperor of China")
       cy.get("img")
   })
+
+  it('Should return home after clicking the back button', () => {
+    cy.visit('http://localhost:3000/337401')
+      .get("button")
+      .click()
+    cy.url().should('eq','http://localhost:3000/')
+  })
+
+  it('Should be rendering AllMovies data from Heroku API', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+      fixture: 'movies.json' 
+    })
+    cy.get("a[name='Money Plane']").find("img").should("have.attr", "src").should("include", "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg")
+    
+  })
+
+
+
+
+
+
+  
   
 
 })
