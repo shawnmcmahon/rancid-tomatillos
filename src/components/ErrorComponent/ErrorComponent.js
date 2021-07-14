c3a82f4322b14ba8a05b4ab705b5834dc56f446d
Bobby Vasquez <shootbv@gmail.com>
2021-07-13 18:53:27 -0600

    Add ErrorComponent that renders when an invalid URL is typed

---
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <h1>404 Error</h1>
+      <h3>The URL you typed was invalid</h3>
+    </div>
+  )
+}
+
+export default ErrorComponent
