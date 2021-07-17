f03f1257c9a9259084ccdf4f09450cfa111b32cb
Bobby Vasquez <shootbv@gmail.com>
2021-07-17 14:02:25 -0600

    Adjust rendering of genres, adjust spacing for mobile sizes, add splash colors to site

--- src/components/MoviePage/InfoContainer/InfoContainer.js
+++ src/components/MoviePage/InfoContainer/InfoContainer.js
@@ -9,14 +9,25 @@ const InfoContainer = ({ title, release, rating, tagline, overview, stylize, run
       <h1>{title} <p className="year">({release.split('-')[0]})</p></h1>
       <Rating rating={rating} />
       {tagline && <h2>{tagline}</h2>}
-      <p>{!overview ? "No overview available" : overview}</p>
+      <p className="overview">{!overview ? "No overview available" : overview}</p>
       <div className="rating-runtime-genre">
         <div className="runtime-rating">
           <p className="runtime"><strong>{runtime}</strong></p>
         </div>
-        <div className="genre-container">
-          {stylize}
-        </div>
+        {stylize.length > 4 &&
+          <div className="genre-container">
+            {stylize}
+          </div>}
+          {stylize.length === 4 &&
+            <div className="medium-container">
+              {stylize}
+            </div>
+          }
+          {stylize.length < 4 &&
+            <div className="smaller-container">
+              {stylize}
+            </div>
+          }
       </div>
       <Link to="/">
         <button>Go Back</button>
